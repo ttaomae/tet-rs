07fe825f5c03bb135122da36653d46d6af18439e
Todd Taomae <ttaomae@gmail.com>
2018-10-05 23:42:00 -1000

    Improve score calculation for SinglePlayerEngine.
    Add the following:
    * Mini T-Spin Single
    * Back-to-back bonus
    * Combo bonus
    * Level based score

--- src/engine/single.rs
+++ src/engine/single.rs
@@ -100,6 +100,20 @@ struct StatTracker {
     score: Cell<u32>,
     last_lock: Cell<TSpin>,
     lines_cleared: Cell<u32>,
+    combo_status: Cell<ComboStatus>,
+    current_combo: Cell<u8>,
+    back_to_back: Cell<bool>,
+}
+
+#[derive(Copy, Clone)]
+enum ComboStatus {
+    /// There is not active combo. The last piece locked did not result in a line clear.
+    Inactive,
+    /// There might be an active combo. A piece was just locked but it is not yet known whether or
+    /// not it will result in a line clear.
+    Maybe,
+    /// There is an active combo. The last piece lock resulted in a line clear.
+    Active,
 }
 
 impl StatTracker {
@@ -108,6 +122,9 @@ impl StatTracker {
             score: Cell::new(0),
             last_lock: Cell::new(TSpin::None),
             lines_cleared: Cell::new(0),
+            combo_status: Cell::new(ComboStatus::Inactive),
+            current_combo: Cell::new(0),
+            back_to_back: Cell::new(false),
         }
     }
 
@@ -119,6 +136,16 @@ impl StatTracker {
 
 impl BaseEngineObserver for StatTracker {
     fn on_lock(&self, t_spin: TSpin) {
+        // Downgrade combo status. It should be reset to Active on line clear.
+        let combo_status = match self.combo_status.get() {
+            ComboStatus::Active => ComboStatus::Maybe,
+            _ => {
+                self.current_combo.set(0);
+                ComboStatus::Inactive
+            },
+        };
+        self.combo_status.set(combo_status);
+
         self.last_lock.set(t_spin);
     }
 
@@ -131,19 +158,39 @@ impl BaseEngineObserver for StatTracker {
     }
 
     fn on_line_clear(&self, n_rows: u8) {
-        self.lines_cleared.set(self.lines_cleared.get() + n_rows as u32);
+        // Increment combo
+        self.combo_status.set(ComboStatus::Active);
+        self.current_combo.set(self.current_combo.get() + 1);
 
-        let mut points = match (n_rows, self.last_lock.get()) {
-            (1, TSpin::Regular) => 800,
-            (2, TSpin::Regular) => 1200,
-            (3, TSpin::Regular) => 1600,
-            (1, _) => 100,
-            (2, _) => 300,
-            (3, _) => 500,
-            (4, _) => 800,
-            (_, _) => 0,
+        let (mut points, back_to_back) = match (n_rows, self.last_lock.get()) {
+            (1, TSpin::None) => (100, false),
+            (2, TSpin::None) => (300, false),
+            (3, TSpin::None) => (500, false),
+            (4, TSpin::None) => (800, true),
+            (1, TSpin::Mini) => (200, true),
+            (1, TSpin::Regular) => (800, true),
+            (2, TSpin::Regular) => (1200, true),
+            (3, TSpin::Regular) => (1600, true),
+            (_, _) => panic!("This should be impossible."),
         };
-        points *= self.get_level() as u32;
-        self.score.set(self.score.get() + points);
+
+        // If we were already in the middle of a back-to-back,
+        // and the current line clear is also a back-to-back ...
+        if self.back_to_back.get() && back_to_back {
+            // Multiply by 1.5. Use * 3 / 2 to avoid casting to f64 then back to u32.
+            // Given the possible values of `points`, this will not result in any truncation.
+            points = points * 3 / 2;
+        }
+
+        self.back_to_back.set(back_to_back);
+
+        // 50 points per combo. 1-combo == 2-in-a-row.
+        points += 50 * (self.current_combo.get() - 1) as u32;
+
+        self.score.set(self.score.get() + points * self.get_level() as u32);
+
+        // Do not update lines cleared until after final score is computed so that level is based on
+        // lines cleared before this current action.
+        self.lines_cleared.set(self.lines_cleared.get() + n_rows as u32);
     }
 }
